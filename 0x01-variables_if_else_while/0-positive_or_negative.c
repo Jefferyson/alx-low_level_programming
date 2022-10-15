0ab3c9eb7d30f3d50d172d7a53ce4c6dcb8acc3a
jefferyson <gokikiola007@gmail.com>
2022-10-15 02:33:10 -0700

    Task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
 /**
  * main-Entry point
  *
- * Return: Always 0 (success)
+ * Return:Always zero (success)
  */
 int main(void)
 {
