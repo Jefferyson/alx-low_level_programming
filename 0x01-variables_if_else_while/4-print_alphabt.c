e0f8aef006f82ce9cf5e2aad1089228833296d99
jefferyson <gokikiola007@gmail.com>
2022-10-15 04:08:54 -0700

    Task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,9 +9,10 @@ int main(void)
 	char x;
 
 	x = 'a';
-	while ((x != 'q' && x != 'e') && x <= 'z')
+	while (x <= 'z')
 	{
-		putchar(x);
+		if ((x != 'q' && x != 'e') && x <= 'z')
+			putchar(x);
 		x++;
 	}
 	putchar('\n');
