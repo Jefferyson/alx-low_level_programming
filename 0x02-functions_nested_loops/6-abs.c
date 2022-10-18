02466f5996726ae7dfe5e7512e29afa7616d830a
jefferyson <gokikiola007@gmail.com>
2022-10-18 12:20:17 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _abs- absolute value of an integer
+ * @r: the int
+ * Return: Absolute value
+ */
+int _abs(int r)
+{
+	if (r >= 0)
+	{
+		return (r);
+	}
+	else
+	{
+		return (-r);
+	}
+}
