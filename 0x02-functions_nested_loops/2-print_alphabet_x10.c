358934b71a1ad5f2d4dd2d7f52a9764031c5c3ab
jefferyson <gokikiola007@gmail.com>
2022-10-18 10:13:03 -0700

    task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print 10 times the alphabets
+ */
+void print_alphabet_x10(void)
+{
+	int y = 0;
+	char x;
+
+	while (y++ <= 9)
+	{
+		for (x = 'a'; x <= 'z'; x++)
+			_putchar(x);
+		_putchar('\n');
+	}
+}
