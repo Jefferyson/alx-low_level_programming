c73f96549292d991e21d4a2074ed21dbfbb9f391
jefferyson <gokikiola007@gmail.com>
2022-10-20 07:51:26 -0700

    3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_numbers - function that prints numbers
+ * 0 to 9
+ *
+ * Return: null
+ */
+
+void print_numbers(void)
+{
+	int x;
+
+	if (x >= '0'; x <= '9')
+	{
+		putchar(x);
+	}
+	putchar('\n');
+}
