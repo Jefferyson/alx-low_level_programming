febffee14783885b076e3afa425c425e81e659f7
jefferyson <gokikiola007@gmail.com>
2022-10-20 07:59:20 -0700

    4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers
+ *
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9')
+	{
+		if (x != '2' && x != '4')
+			_putchar(c);
+	}
+	_putchar('\n');
+}
