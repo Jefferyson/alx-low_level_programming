b386905f044fa65519cc972d81fc8d672d1e71ae
jefferyson <gokikiola007@gmail.com>
2022-10-20 07:30:10 -0700

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ * @x: character to check
+ *
+ * Return: 1 or 0
+ */
+int _isupper(int x)
+{
+	if (x >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
