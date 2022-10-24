32b80d956c7c6429bcc0841044cd4030a648f6fa
jefferyson <gokikiola007@gmail.com>
2022-10-24 15:29:34 -0700

    4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,12 +12,12 @@ void print_rev(char *s)
 	int c, y;
 
 	y = 0;
-	while (*(s + y) != '\0')
+	while (s[y] != '\0')
 		y++;
 
 	for (c = y - 1; c >= 0; c--)
 	{
-		_putchar(*(s + y));
+		_putchar(s[c]);
 	}
 	_putchar('\n');
 }
