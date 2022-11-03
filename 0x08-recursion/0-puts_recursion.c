c8dbdf1b03fa914b986d7734081dced7eb7cb012
jefferyson <gokikiola007@gmail.com>
2022-11-03 15:02:00 -0700

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - function that prints a string
+ * @s: the string to be printed
+ */
+void _puts_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+}
